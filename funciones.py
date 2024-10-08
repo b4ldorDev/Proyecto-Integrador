c2ac6ec2792011226c028431bc69b750d98fd60d
Ileana T. <98206306+b4ldorIMT@users.noreply.github.com>
2024-10-08 09:59:59 -0600

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -1,11 +1,11 @@
 def tipo_dato(cadena):
-    especiales=[]
+    especiales = []
     num = [x for x in cadena if x.isnumeric() == True]
     alpha = [x for x in cadena if x.isalpha() == True]
     alphanumeric = [x for x in cadena if x.isalnum() == True]
-    especiales = [x for x in cadena if x.isalnum() == False and x.isnumeric==False and x.isalpha()== False]
+    especiales = [x for x in cadena if x.isalnum() == False and x.isnumeric == False and x.isalpha() == False]
 
-    if (num==0 and alpha ==0 and alphanumeric==0 and especiales==0 ):
+    if (num == 0 and alpha == 0 and alphanumeric == 0 and especiales == 0):
         print("Los datos son nulos")
     if len(cadena) == len(num):
         print("Todos los caracteres son numeros:")
@@ -15,26 +15,30 @@ def tipo_dato(cadena):
         print("Todos los caracteres son alfanumericos:")
     if len(cadena) == len(especiales):
         print("Todos los caracteres son especiales ")
-    else:              
+    else:
         print(f"No todos los caracteres son números solo contiene {len(num)} y son {num}")
         print(f"No todos los caracteres son números solo contiene {len(alpha)} y son {alpha}")
         print(f"No todos los caracteres son números solo contiene {len(alphanumeric)} y son {alphanumeric}")
         print(f"No todos los caracters son especiales solo contiene {len(especiales)} y son {especiales}")
 
 def csv_a_arreglo(archivo):
-    arreglo=[]
-    arch=open(archivo,"r")
+    count_rows= 0
+    count_cols = 0
+    arreglo = []
+    arch = open(archivo, "r")
     for reg in arch.readlines():
         columna = [1]
-        lista=reg.split(",")
+        count_rows +=1
+        lista = reg.split(",")
         for elemento in lista:
+            count_cols +=1
             columna.append(elemento)
-        arreglo.append(columna) 
-    return arreglo   
-        
-#def cantidad_de_campos():
+        arreglo.append(columna)
+    return arreglo
 
-#def cantidad_de_registros():
 
-#def existencia():
-    
+# def cantidad_de_campos():
+
+# def cantidad_de_registros():
+
+# def existencia():

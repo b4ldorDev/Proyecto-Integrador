84944d1ca4be324ecdfe24344bc4dfe01894468f
LanBeastt <89526563+LanBeastt@users.noreply.github.com>
2024-10-08 17:28:34 -0600

    Update_1.1 proyecto_A2.py

--- proyecto_A2.py
+++ proyecto_A2.py
@@ -8,4 +8,38 @@ def requerimiento_2a(datos):
     while i<campos:
         columna=encabezado[i]
         datcol=[fila[i] for fila in datos[1:]]
-        
+        numero=True
+        for val in datcol:
+            if val.isdigit()==True:
+                numero=True
+            else:
+                numero=False
+        letra=True
+        for val in datcol:
+            if val.isalpha()==True:
+                letra=True
+            else:
+                letra=False
+        alphanum=True
+        for val in datcol:
+            if val.isalnum()==True:
+                alphanum=True
+            else:
+                alphanum=False
+        especiales=True
+        for val in datcol:
+            if val.isalnum()==False:
+                especiales=True
+            else:
+                especiales=False
+        vacios=0
+        for val in datcol:
+            if val==" ":
+                vacios+=1
+        procentaje=(vacios/registros)*100
+        print(f"Columna: {columna}")
+        print(f"Numéricos: {numero}")
+        print(f"Alfabéticos: {letra}")
+        print(f"Alfanuméricos: {alphanum}")
+        print(f"Especiales: {especiales}")
+        print(f"Porcentaje de datos vacios: ",procentaje"%")
